b14950db7eec18a7db536f0635e10b9a605fdda3
Rafail Moraitis <nemrafmor@gmail.com>
2023-12-14 15:39:54 +0200

    Models

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from forms import CreatePostForm, RegisterForm, LoginForm ,CommentForm
 from flask_gravatar import Gravatar
 from functools import wraps
 from dotenv import load_dotenv
+from models import User , BlogPost, Comment
 import os
 
 load_dotenv()
@@ -34,46 +35,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
-
-
-
-##CONFIGURE TABLES
-
-class User(UserMixin, db.Model):
-    __tablename__ = "users"
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(100))
-    posts = db.relationship("BlogPost", back_populates="author")
-    comments = db.relationship("Comment", back_populates="comment_author")
-
-
-class BlogPost(db.Model):
-    __tablename__ = "blog_posts"
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    author = db.relationship("User", back_populates="posts")
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    subtitle = db.Column(db.String(250), nullable=False)
-    date = db.Column(db.String(250), nullable=False)
-    body = db.Column(db.Text, nullable=False)
-    img_url = db.Column(db.String(250), nullable=False)
-    
-    #***************Parent Relationship*************#
-    comments = db.relationship("Comment", back_populates="parent_post")
-
-
-class Comment(db.Model):
-    __tablename__ = "comments"
-    id = db.Column(db.Integer, primary_key=True)
-    author_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    comment_author = db.relationship("User", back_populates="comments")
-    
-    #***************Child Relationship*************#
-    post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
-    parent_post = db.relationship("BlogPost", back_populates="comments")
-    text = db.Column(db.Text, nullable=False)
 
 db.create_all()
 
